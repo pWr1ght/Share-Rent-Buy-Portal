afc73f7313a7a585d74d556a7edfa3a6514d9c5f
tbara <tbarabasz@noblyapps.net>
2020-04-11 15:33:57 -0700

    Project template

---
+++ main.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var mysql = require('./modules/dbcon.js');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express();
+var handlebars = require('express-handlebars').create({
+        defaultLayout:'main',
+        });
+
+// configure the app to use bodyParser()
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.engine('handlebars', handlebars.engine);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/static', express.static('public')); //Allow use of static files
+app.set('view engine', 'handlebars'); //Use Handlebars templates
+app.set('port', process.argv[2]);  //Accept port from the commandline 
+app.set('mysql', mysql);  //Use MySql DB engine
+app.use(express.static(path.join(__dirname, '/public'))); //Specify static files routes
+app.use(express.static(path.join(__dirname, '/uploads')));
+
+//Specify routes
+
+app.use('/', require('./routes/home.js'));
+
+//Go here when 404
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+//Go here when 500 error
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+//Server
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
---
+++ routes/home.js
@@ -0,0 +1,18 @@
+module.exports = function () {
+    var express = require('express');
+    var router = express.Router();
+
+    /* This function extracts the current non-expired listings and renders the home page with the data.*/
+    function serveHome(req, res, next) {
+        var context={
+            "resp": "Your home page"
+        }
+        res.render('home', context);
+
+    }
+
+
+    /* The routes for homepage */
+    router.get('/', serveHome); 
+    return router;
+}();
