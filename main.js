2956c61340cfe1e7968e9c606dca30712e6d29e1
Andres Zourelli <azourelli@gmail.com>
2020-04-11 19:59:15 -0700

    fixing deployment

--- main.js
+++ main.js
@@ -4,20 +4,22 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var app = express();
 var handlebars = require('express-handlebars').create({
-        defaultLayout:'main',
-        });
+	defaultLayout: 'main'
+});
 
 // configure the app to use bodyParser()
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+	bodyParser.urlencoded({
+		extended: true
+	})
+);
 app.use(bodyParser.json());
 app.engine('handlebars', handlebars.engine);
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/static', express.static('public')); //Allow use of static files
 app.set('view engine', 'handlebars'); //Use Handlebars templates
-app.set('port', process.argv[2] || 3344 );  //Accept port from the commandline 
-app.set('mysql', mysql);  //Use MySql DB engine
+app.set('port', process.env.PORT || process.argv[2] || 3344); //Accept port from the commandline
+app.set('mysql', mysql); //Use MySql DB engine
 app.use(express.static(path.join(__dirname, '/public'))); //Specify static files routes
 app.use(express.static(path.join(__dirname, '/uploads')));
 
@@ -26,19 +28,19 @@ app.use(express.static(path.join(__dirname, '/uploads')));
 app.use('/', require('./routes/home.js'));
 
 //Go here when 404
-app.use(function(req,res){
-  res.status(404);
-  res.render('404');
+app.use(function(req, res) {
+	res.status(404);
+	res.render('404');
 });
 
 //Go here when 500 error
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500);
-  res.render('500');
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
 });
 
 //Server
-app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+app.listen(app.get('port'), function() {
+	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
