a37b9eec08dcf4c8aa4dce9651bca64ddf996d37
yeeda <52306078+CS290yeeda@users.noreply.github.com>
2020-05-13 00:39:20 -0700

    Minor code change and edit category bug fix

--- public/scripts/edititem.js
+++ public/scripts/edititem.js
@@ -6,78 +6,67 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function saveEdit() {
     //modal for saving screen
-    document.getElementById("loadingModalStatus").textContent = "Saving new listing...";
+    let loadingModal = document.getElementById("loadingModalStatus");
+    loadingModal.textContent = "Saving new listing...";
     $("#LoadingModal1").modal();
 
-    console.log("Saving edit......");
-    let itemId = document.getElementById("edit_item_Id").value;
-    let title = document.getElementById("edit_name").value;
-    let descr = document.getElementById("edit_descr").value;
-    let price = document.getElementById("edit_price").value;
-    let category = document.getElementById("inputCat").value;
-    let street = document.getElementById("inputAddress").value;
-    let city = document.getElementById("inputCity").value;
-    let state = document.getElementById("inputState").value;
-    let zip = document.getElementById("inputZip").value;
-    let newfilelist = document.getElementById("myFile").files;
+    let payload = getFormFields();
 
-    let item = {
-        itemID: itemId,
-        catID: category,
-        itemName: title,
-        itemDescription: descr,
-        itemPrice: price,
-        itemAddress: street,
-        itemCity: city,
-        itemState: state,
-        itemZip: zip,
-    }
-
-    var req = new XMLHttpRequest();
-    req.open('POST', '/editItem/save', true)
-    req.setRequestHeader('Content-Type', 'application/json');
-    req.addEventListener('load', function () {
-        //Process response
-        if (req.status >= 200 && req.status < 400) {
-            var resp = req.responseText;
-            console.log(resp);
-            
-            //upload and delete attachments
-            fetch('/aws/delete', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({deleteList:gatherDeleteAttachments("close")})
-            })
-                .then((data1) => {
-                    return data1.json();           
-                }).then((data2) => {
-                    console.log(data2);
-                    //upload attachment
-                    document.getElementById("loadingModalStatus").textContent = "Updating attachment...";
-                    var form = document.forms.namedItem("fileinfo");
-                    var formData = new FormData(form);
-                    formData.append("listId",itemId);
-                    formData.append("newFileCt",newfilelist.length);
-                    return fetch('/aws/upload', {method: 'POST', body: formData});				
-                }).then((data3) => {
-                    return data3.json();
-                }).then((data4) => {
-                    console.log(data4);
-                    document.getElementById("loadingModalStatus").textContent = data4.uploadStatus;
-                    setTimeout(function(){
-                        location.reload();
-                    },2000);
-                    
-                    return data4; 
-                })
-                .catch((err) => console.log(err));  
-
-        } else {
-            console.log("Error in network request: " + req.statusText);
-        }
-    });
-    req.send(JSON.stringify(item));
+    fetch('editItem/save',{
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload)
+    }).then((response)=>{
+        if(!response.ok){throw Error(response.status);}
+        //delete attachments
+        return fetch('/aws/delete', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({deleteList:gatherDeleteAttachments("close")})});           
+    }).then((response1) => {
+        if(!response1.ok){throw Error(response1.status);}
+        return response1.json();          
+    }).then((data2) => {
+        //upload attachment
+        loadingModal.textContent = "Updating attachment...";
+        var form = document.forms.namedItem("fileinfo");
+        var formData = new FormData(form);
+        formData.append("listId",payload.itemID);
+        formData.append("newFileCt",document.getElementById("myFile").files.length);
+        return fetch('/aws/upload', {method: 'POST', body: formData});				
+    }).then((data3) => {
+        return data3.json();
+    }).then((data4) => {
+        loadingModal.textContent = data4.uploadStatus;
+        reloadPage(2000);                   
+        return data4; 
+    }).catch((err) => {
+        loadingModal.textContent = err;
+        reloadPage(2000);
+    });  
 }
+
+
+function getFormFields(){
+    return {
+        itemID: document.getElementById("edit_item_Id").value,
+        catID: document.getElementById("inputCat").value,
+        itemName: document.getElementById("edit_name").value,
+        itemDescription: document.getElementById("edit_descr").value,
+        itemPrice: document.getElementById("edit_price").value,
+        itemAddress: document.getElementById("inputAddress").value,
+        itemCity: document.getElementById("inputCity").value,
+        itemState: document.getElementById("inputState").value,
+        itemZip: document.getElementById("inputZip").value,
+    }
+}
+
+function reloadPage(timeout){
+    setTimeout(function(){
+        location.reload();
+    },timeout);
+}
+
 
 function cancelEdit() {
     document.getElementById("edits").hidden = true;
@@ -85,7 +74,7 @@ function cancelEdit() {
     document.getElementById("edit_name").value = "";
     document.getElementById("edit_descr").value = "";
     document.getElementById("edit_price").value = "";
-    document.getElementById("inputCat").value = "";
+    document.getElementById("inputCat").innerHTML = "";
     document.getElementById("inputAddress").value = "";
     document.getElementById("inputCity").value = "";
     document.getElementById("inputState").value = "";
@@ -97,7 +86,7 @@ function cancelEdit() {
     document.getElementById("myFile").value = null;
 }
 
-function populateCategories(data) {
+function populateCategoriesAndSelect(data) {
     let category = document.getElementById("inputCat");
     var req = new XMLHttpRequest();
     req.open('GET', '/editItem/cats', true);
@@ -125,55 +114,82 @@ function populateCategories(data) {
     req.send(null);
 }
 
-function populateEditForm(data) {
-    let itemId = document.getElementById("edit_item_Id");
-    let title = document.getElementById("edit_name");
-    let descr = document.getElementById("edit_descr");
-    let price = document.getElementById("edit_price");
-    let category = document.getElementById("inputCat");
-    let street = document.getElementById("inputAddress");
-    let city = document.getElementById("inputCity");
+
+function renderEditFormWithData(data){
+    loadFormFields(data);
+    loadAttachments(data.attachments);
+}
+
+
+function loadFormFields(data){
+    //fill in the form fields
+    fillTextFieldsWithData(data);
+
+    //set the state
     let state = document.getElementById("inputState");
-    let zip = document.getElementById("inputZip");
-    let imgs = document.getElementById("attachImgs");
-
-    title.focus();
-    itemId.value = data.id;
-    title.value = data.itemName;
-    descr.value = data.itemDescription;
-    price.value = data.itemPrice;
-    street.value = data.itemAddress;
-    city.value = data.itemCity;
-    zip.value = data.itemZip;
-
     for (var i = 0; i < state.options.length; i++) {
         if (state.options[i].value == data.itemState) {
             state.options[i].selected = true;
         }
     }
-
-    for(let n = 0; n < data.attachments.length; n++){
-        let tmp = document.createElement("img");
-        tmp.className = "img-thumbnail";
-        tmp.setAttribute("id", "att_"+data.attachments[n].AttachId);
-        tmp.setAttribute("src", data.attachments[n].AttLink);
-        
-        let tmp2 = document.createElement("span");
-        tmp2.setAttribute("id", "attname_"+data.attachments[n].AttachId);
-        tmp2.setAttribute("data-filename", data.attachments[n].AttName);
-        tmp2.setAttribute("data-id", data.attachments[n].AttachId);
-        tmp2.setAttribute("data-deleted", '0');
-        tmp2.className = "close";
-        tmp2.textContent = "X";
-        let tmp3 = document.createElement("div");
-        tmp3.className = "img-wrap col-sm-4";
-        tmp3.appendChild(tmp2);
-        tmp3.appendChild(tmp);
-        imgs.appendChild(tmp3);
-    }
-    addImageRemoveFunction();
-    populateCategories(data);
+    
+    //fill in and set the category
+    populateCategoriesAndSelect(data);
 }
+
+
+function fillTextFieldsWithData(data){
+    document.getElementById("edit_item_Id").value = data.id;
+    document.getElementById("edit_name").focus();
+    document.getElementById("edit_name").value = data.itemName;
+    document.getElementById("edit_descr").value = data.itemDescription;
+    document.getElementById("edit_price").value = data.itemPrice;
+    document.getElementById("inputAddress").value = data.itemAddress;
+    document.getElementById("inputCity").value = data.itemCity;
+    document.getElementById("inputZip").value = data.itemZip;
+}
+
+
+function loadAttachments(attachmentArr){
+    let imgtags = document.getElementById("attachImgs");
+    //write the html for each attachment
+    for(let n = 0; n < attachmentArr.length; n++){
+
+        let curImg = createImgElement(attachmentArr[n]);      
+        let curImgData = createImgDataElement(attachmentArr[n]);
+        let curImgContainer = document.createElement("div");
+        curImgContainer.className = "img-wrap col-sm-4";
+        curImgContainer.appendChild(curImgData);
+        curImgContainer.appendChild(curImg);
+        imgtags.appendChild(curImgContainer);
+    }
+    
+    //add the image remove function to each image and toggle attachment link and attachment block
+    addImageRemoveFunction();
+    toggleAttachLnkAndAttachBlock();
+}
+
+
+function createImgElement(imgData){
+    let curImg = document.createElement("img");
+    curImg.className = "img-thumbnail";
+    curImg.setAttribute("id", "att_"+imgData.AttachId);
+    curImg.setAttribute("src", imgData.AttLink);
+    return curImg;
+}
+
+
+function createImgDataElement(imgData){
+    let curImgData = document.createElement("span");
+    curImgData.setAttribute("id", "attname_"+imgData.AttachId);
+    curImgData.setAttribute("data-filename", imgData.AttName);
+    curImgData.setAttribute("data-id", imgData.AttachId);
+    curImgData.setAttribute("data-deleted", '0');
+    curImgData.className = "close";
+    curImgData.textContent = "X";
+    return curImgData;
+}
+
 
 function editItem(id) {
     document.getElementById("edits").hidden = false;
@@ -186,7 +202,7 @@ function editItem(id) {
         //Process response
         if (req.status >= 200 && req.status < 400) {
             var resp = JSON.parse(req.responseText);
-            populateEditForm(resp);
+            renderEditFormWithData(resp);
         } else {
             console.log("Error in network request: " + req.statusText);
         }
@@ -229,8 +245,7 @@ function deleteItem(id) {
     }
 }
 
-//This function attaches the 'x' to the pictures and
-//sets up the logic for displaying various attachment components
+
 function addImageRemoveFunction(){
     //reference from stackoverflow
     var closeBtns = document.querySelectorAll('.img-wrap .close')
@@ -239,34 +254,31 @@ function addImageRemoveFunction(){
         closeBtns[i].addEventListener('click', function() {
             this.setAttribute("data-deleted",'1');
             this.parentElement.style.display = "none";
-            //var imgWrap = this.parentElement;
-            //imgWrap.parentElement.removeChild(imgWrap);
-            let imgWrapList = document.getElementsByClassName("img-wrap");
-            let curDeletelist = gatherDeleteAttachments("close");
-            if(imgWrapList.length-curDeletelist.files.length > 0){
-                document.getElementById("curAttached").style.display = "block";
-            }else{
-                document.getElementById("curAttached").style.display = "none";
-            }
-            
-            document.getElementById("addAttachLnk").disabled = false;
-            
+            toggleAttachLnkAndAttachBlock();           
         });
     }
+}
 
-    //disable add attachment link
+
+function toggleAttachLnkAndAttachBlock(){
     let imgWrapList = document.getElementsByClassName("img-wrap");
-    if(imgWrapList.length == 3){
-        document.getElementById("addAttachLnk").disabled = true;
-    }else{
-        document.getElementById("addAttachLnk").disabled = false;
-    }
-    if(imgWrapList.length > 0){
+    let curDeletelist = gatherDeleteAttachments("close");
+
+    //toggle Attach Block
+    if(imgWrapList.length-curDeletelist.files.length > 0){
         document.getElementById("curAttached").style.display = "block";
     }else{
         document.getElementById("curAttached").style.display = "none";
     }
+
+    //toggle New Attachment Link
+    if(imgWrapList.length-curDeletelist.files.length == 3){
+        document.getElementById("addAttachLnk").disabled = true;
+    }else{
+        document.getElementById("addAttachLnk").disabled = false;
+    }
 }
+
 
 //This function gathers the attachments to be deleted
 function gatherDeleteAttachments(imgtagClassName){
