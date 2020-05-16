51252d8b7cf0c94e26ef4f1001c1573be8da0eaf
yeeda <52306078+CS290yeeda@users.noreply.github.com>
2020-05-16 14:59:27 -0700

    Add Input Validation to Add New item page.

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -15,43 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	})
 });
 
-/*
-function getDistances() {
-	var ids = document.getElementsByName('elemId').length;
-	console.log('ids: ' + ids);
-	for (let index = 1; index < ids + 1; index++) {       //Need to fix, the id doesn't necessarily start from 1
-		getDist(index);
-	}
-}
-
-function getDist(id) {
-	var destLat = document.getElementById(id + '-lat').innerHTML;
-	var destLon = document.getElementById(id + '-lon').innerHTML;
-	var userLat = document.getElementById('user_lat').value;
-	var userLon = document.getElementById('user_lon').value;
-
-	payload = {
-		destLat: destLat,
-		destLon: destLon,
-		userLat: userLat,
-		userLon: userLon
-	};
-
-	var req = new XMLHttpRequest();
-	req.open('POST', '/locate', true);
-	req.setRequestHeader('Content-Type', 'application/json');
-	req.addEventListener('load', function() {
-		//Process response
-		if (req.status >= 200 && req.status < 400) {
-			var resp = JSON.parse(req.responseText);
-			document.getElementById(id + '-dist').innerText = resp.distText + '    |    ' + resp.durText;
-		} else {
-			console.log('Error in network request: ' + req.statusText);
-		}
-	});
-	req.send(JSON.stringify(payload));
-}
-*/
 function getDistances2(){
 	let userCoord = {lat: document.getElementById('user_lat').value, long: document.getElementById('user_lon').value};
 	let items = document.getElementsByClassName("LatLong");
