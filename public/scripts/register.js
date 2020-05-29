59081a878cc16b287f46cacd4dcea015f740c00a
tbara <tbarabasz@noblyapps.net>
2020-05-28 22:49:27 -0700

    password validation

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,23 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("registerBtn").disabled = true;
-    /*
-    document.getElementById("registerBtn").addEventListener("click", ()=>{
-        event.preventDefault();
-    });*/
     document.getElementById("edit_firstname").focus();
+
 });
 
+const pass_match = document.getElementById("edit_pwd");
 const pass = document.getElementById("edit_password");
 const pattern = pass.getAttribute('pattern');
 var reg_ex = new RegExp(pattern)
 
 pass.onkeyup = function() {
-    if (reg_ex.test(pass.value)) {
+    checkPassword();
+}
+
+pass_match.onkeyup = function() {
+    checkPassword();
+}
+
+function checkPassword() {
+    if (reg_ex.test(pass.value) && matchPassword()) {
         document.getElementById("registerBtn").disabled = false;
         document.getElementById("passwordOK").style.display = "block";
+        document.getElementById("edit_password_label").style.color = "rgb(1, 190, 1)";
     }else{
         document.getElementById("registerBtn").disabled = true;
         document.getElementById("passwordOK").style.display = "none";
+        document.getElementById("edit_password_label").removeAttribute("style");
     }
 }
 
@@ -26,3 +34,13 @@ function registerUser(){
         console.log("yeah")
     }
 }
+
+function matchPassword() { 
+    let password1 = document.getElementById("edit_pwd").value;
+    let password2 = document.getElementById("edit_password").value;
+    if (password1 == password2) {
+        return true;
+    } else {
+        return false;
+    }
+} 
